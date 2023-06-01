6e4c362449518d7ec40a8b555d23de36737f4e34
Richard <richerd.rdc@gmail.com>
2023-06-01 12:36:04 -0700

    style: add missing semicolons

--- src/lapis/Collection.d.ts
+++ src/lapis/Collection.d.ts
@@ -2,15 +2,15 @@ import { CollectionOptions, CollectionSchema } from ".";
 import Document from "./Document";
 
 declare class Collection<T extends CollectionSchema> {
-    private dataStore: DataStore
-    private options: CollectionOptions<T> 
-    private openDocuments: { [index: string]: Promise<Document<T>> }
+    private dataStore: DataStore;
+    private options: CollectionOptions<T>;
+    private openDocuments: { [index: string]: Promise<Document<T>> };
 
     // as I'm using export =, I can only export one thing.
     // therefore, I'm importing CollectionOptions from index.d.ts where it is also referenced
-    constructor(name: string, options: CollectionOptions<T>)
+    constructor(name: string, options: CollectionOptions<T>);
 
-    load(key: string): Promise<Document<T>>
+    load(key: string): Promise<Document<T>>;
 }
 
 export = Collection;
